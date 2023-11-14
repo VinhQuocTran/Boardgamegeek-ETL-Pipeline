cf23f5ee135cdc1c082e05483ab0f0076f7b5e18
VinhQuocTran <vinhquoc2103@gmail.com>
2023-11-14 20:13:22 +0700

    Final Version Update

--- src/py/boardgamegeek_fa/silver_tranformation/__init__.py
+++ src/py/boardgamegeek_fa/silver_tranformation/__init__.py
@@ -8,6 +8,7 @@ import pandas as pd
 import os
 import sys
 import io
+import ast
 from urllib.parse import urlparse
 import xml.etree.ElementTree as ET
 
@@ -17,40 +18,20 @@ sys.path.append(dir_path)
 from utility_function import get_current_hour
 from utility_function import init_adls
 
-def categoize_text_difficulty(desc):
-    if(desc=="No necessary in-game text"):
-        return 1
-    elif(desc=="Some necessary text - easily memorized or small crib sheet"):
-        return 2
-    elif(desc=="Moderate in-game text - needs crib sheet or paste ups"):
-        return 3
-    elif(desc=="Extensive use of text - massive conversion needed to be playable"):
-        return 4
-    return 5
 
-
+############## Main function ##############
 def main(inputBlob: InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {inputBlob.name}\n"
                  f"Blob Size: {inputBlob.length} bytes")
     
-
-    # Reading consolidated_game_info.json using pd.read_json
-    # blob_content = inputBlob.read()
-    # json_file_like = io.BytesIO(blob_content)
-    # silver_df = pd.read_json(json_file_like)
-
-    # Reading consolidated_game_info.json using pd.DataFrame
-    # blob_content = inputBlob.read().decode("utf-8")
-    # json_data = json.loads(blob_content)
-    # silver_df = pd.DataFrame(json_data)
-
     # Read data from inputBlob and convert to DataFrame
     blob_content = inputBlob.read()
     csv_file_like = io.StringIO(blob_content.decode('utf-8'))
     silver_df = pd.read_csv(csv_file_like,encoding="utf-8")
 
     # add total_award column
+    silver_df['boardgamehonor'] = silver_df['boardgamehonor'].apply(ast.literal_eval)
     silver_df['total_award']=silver_df['boardgamehonor'].str.len()
     silver_df['total_award'] = silver_df['total_award'].fillna(0)
 
@@ -67,6 +48,15 @@ def main(inputBlob: InputStream):
     adls.upload_file_to_container(container_name,csv_content,"silver_game_info.csv")
 
 
-
-
+############## Custom function ##############
+def categoize_text_difficulty(desc: str) -> int:
+    if(desc=="No necessary in-game text"):
+        return 1
+    elif(desc=="Some necessary text - easily memorized or small crib sheet"):
+        return 2
+    elif(desc=="Moderate in-game text - needs crib sheet or paste ups"):
+        return 3
+    elif(desc=="Extensive use of text - massive conversion needed to be playable"):
+        return 4
+    return 5
 
--- src/sql/TestDatabase.sql
+++ src/sql/TestDatabase.sql
@@ -1,8 +1,8 @@
+-- SQL query for testing the database
+-- Get game's data
+select * from boardgame
 
-SELECT name FROM master.dbo.sysdatabases;
-
-select * from boardgamepublisher
-
+-- Count game publised by publisher
 select count(game_id) as game_published,boardgamepublisher_name from bridge_boardgamepublisher brige_bga
 inner join boardgamepublisher bga on bga.boardgamepublisher_id=brige_bga.boardgamepublisher_id
 group by brige_bga.boardgamepublisher_id,boardgamepublisher_name
